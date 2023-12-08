76227294e8c3496a39cb5f8d454ce71bbe322946
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2023-12-08 22:29:03 +0200

    Script to get endpoint metrics

--- serverless/get_endpoint_metrics.py
+++ serverless/get_endpoint_metrics.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     args = get_args()
     runpod = runpod.Serverless()
 
-    response = runpod.get_serverless_metrics(args.endpoint_id)
+    response = runpod.get_serverless_request_metrics(args.endpoint_id)
 
     if response.status_code == 200:
         resp_json = response.json()
@@ -34,25 +34,54 @@ if __name__ == '__main__':
             for error in resp_json['errors']:
                 print(error['message'])
         else:
-            print(json.dumps(resp_json, indent=4, default=str))
-            # table = PrettyTable(padding_width=2)
-            # table.field_names = ['Status', 'Request ID', 'Delay Time', 'Execution Time', 'Worker ID']
-            # table.align = 'l'
-            #
-            # if 'requests' in resp_json and resp_json['requests'] is not None:
-            #     for r in resp_json['requests']:
-            #         table.add_row([
-            #             r['status'],
-            #             r['id'],
-            #             r['delayTime'],
-            #             r['executionTime'],
-            #             r['workerId']
-            #         ])
-            #
-            #     print(table)
-            # else:
-            #     print('No requests')
+            table = PrettyTable(padding_width=2)
+            table.field_names = [
+                'Time',
+                'Jobs',
+                'C',
+                'F',
+                'R',
+                'DT Max',
+                'DT Min',
+                'DT Total',
+                'DT N95',
+                'DT P70',
+                'DT P90',
+                'DT P98',
+                'ET Max',
+                'ET Min',
+                'ET Total',
+                'ET N95',
+                'ET P70',
+                'ET P90',
+                'ET P98'
+            ]
+            table.align = 'l'
 
+            for row in resp_json['data']:
+                table.add_row([
+                    row['time'],
+                    row['requests'],
+                    row['completed_requests'],
+                    row['failed_requests'],
+                    row['retried'],
+                    row['dt_max'],
+                    row['dt_min'],
+                    row['dt_total'],
+                    row['dt_n95'],
+                    row['dt_p70'],
+                    row['dt_p90'],
+                    row['dt_p98'],
+                    row['et_max'],
+                    row['et_min'],
+                    row['et_total'],
+                    row['et_n95'],
+                    row['et_p70'],
+                    row['et_p90'],
+                    row['et_p98']
+                ])
+
+            print(table)
     elif response.status_code == 401:
         print('ERROR: Unauthorized (401) - Check your API token')
     else:
